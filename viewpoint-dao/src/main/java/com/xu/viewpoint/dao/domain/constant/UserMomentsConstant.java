30829bc1c3ad8f516d33086c835ebe9a0842df45
jingxuyy <jingxuyy@163.com>
2024-11-24 19:53:44 +0800

    引入redis和RocketMQ，完成对应配置以及工具类，创建用户动态表

---
+++ viewpoint-dao/src/main/java/com/xu/viewpoint/dao/domain/constant/UserMomentsConstant.java
@@ -0,0 +1,19 @@
+package com.xu.viewpoint.dao.domain.constant;
+
+/**
+ * @author: xuJing
+ * @date: 2024/11/24 17:54
+ */
+
+public class UserMomentsConstant {
+
+    /**
+     * RocketMQ的分组名
+     */
+    public static final String GROUP_MOMENTS = "DynamicMomentsGroup";
+
+    /**
+     * 订阅名称
+     */
+    public static final String TOPIC_MOMENTS = "Topic-Moments";
+}
---
+++ viewpoint-service/src/main/java/com/xu/viewpoint/service/config/RocketMQConfig.java
@@ -0,0 +1,64 @@
+package com.xu.viewpoint.service.config;
+
+import com.xu.viewpoint.dao.domain.constant.UserMomentsConstant;
+import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
+import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.common.message.MessageExt;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.core.RedisTemplate;
+
+import java.util.List;
+
+/**
+ * @author: xuJing
+ * @date: 2024/11/24 17:47
+ */
+@Configuration
+public class RocketMQConfig {
+
+    @Value("${rocketmq.name.server.address}")
+    private String nameServerAddr;
+
+    @Autowired
+    private RedisTemplate<String, String> redisTemplate;
+
+    /**
+     * 生产者
+     */
+    @Bean("momentsProducer")
+    public DefaultMQProducer momentsProducer() throws MQClientException {
+        DefaultMQProducer producer = new DefaultMQProducer(UserMomentsConstant.GROUP_MOMENTS);
+        producer.setNamesrvAddr(nameServerAddr);
+        producer.start();
+        return producer;
+    }
+
+
+    /**
+     * 消费者
+     */
+    @Bean("momentsConsumer")
+    public DefaultMQPushConsumer momentsConsumer() throws MQClientException {
+        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer(UserMomentsConstant.GROUP_MOMENTS);
+        consumer.setNamesrvAddr(nameServerAddr);
+        consumer.subscribe(UserMomentsConstant.TOPIC_MOMENTS, "*");
+        consumer.registerMessageListener(new MessageListenerConcurrently() {
+            @Override
+            public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> list, ConsumeConcurrentlyContext consumeConcurrentlyContext) {
+                for (MessageExt msg : list) {
+                    System.out.println(msg);
+                }
+                return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+            }
+        });
+        consumer.start();
+        return consumer;
+    }
+}
